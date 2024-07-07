d4d5daa706a3e83fcbcd9510d70632fc6ec74980
Nemoy123 <alipkin@yandex.ru>
2024-07-07 13:52:42 +0300

    review1

---
+++ sprint1/problems/final_task/solution/src/json_loader.cpp
@@ -0,0 +1,193 @@
+#include "json_loader.h"
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <boost/json/src.hpp>
+
+namespace json_loader {
+    namespace json = boost::json;
+    using namespace std::literals;
+    using namespace model;
+
+std::vector<Road> ParseRoads (const json::array& arr) {
+    std::vector<Road> result;
+    result.reserve(arr.size());
+    for (const auto& road : arr) {
+        int x0_coordinate = 0;
+        int y0_coordinate = 0;
+        int x1_coordinate = 0;
+        int y1_coordinate = 0;
+        bool vert = false;
+        for (const auto& [key, val]:road.as_object()) {
+            if (key == "x0") { x0_coordinate = val.as_int64();}  
+            if (key == "y0") { y0_coordinate = val.as_int64();}
+            if (key == "y1") { 
+                y1_coordinate = val.as_int64();
+                vert = true;
+            }
+            if (key == "x1") { x1_coordinate = val.as_int64();}
+        }
+        if (vert) {
+            result.emplace_back (Road (Road::VERTICAL, Point {x0_coordinate, y0_coordinate},  y1_coordinate));
+        } else {
+            result.emplace_back (Road (Road::HORIZONTAL, Point {x0_coordinate, y0_coordinate},  x1_coordinate));
+        }
+    }
+    return result;
+}
+
+std::vector<Building> ParseBuildings (const json::array& arr) { 
+    std::vector<Building> result;
+    result.reserve(arr.size());
+    for (const auto& building : arr) { 
+        int x = 0;
+        int y = 0;
+        int h = 0;
+        int w = 0;
+        for (const auto& [key, val]:building.as_object()) {
+            if (key == "x") { x = val.as_int64();} 
+            if (key == "y") { y = val.as_int64();}
+            if (key == "h") { h = val.as_int64();}
+            if (key == "w") { w = val.as_int64();}
+        }
+        result.emplace_back ( Building ( Rectangle { Point{x,y}, Size{w,h}} ));
+    }
+    return result;
+}
+//
+std::vector<Office> ParseOffices (const json::array& arr) {  
+    std::vector<Office> result;
+    result.reserve(arr.size());
+    for (const auto& off : arr) {  
+        std::string id {};
+        int x = 0, y = 0, offsetx = 0, offsety = 0;
+        for (const auto& [key, val]:off.as_object()) {
+            if (key == "x") { x = val.as_int64();} 
+            if (key == "y") { y = val.as_int64();}
+            if (key == "offsetX") { 
+                offsetx = val.as_int64();
+                }
+            if (key == "offsetY") { offsety = val.as_int64();}
+            if (key == "id") { id = val.as_string();} 
+        }
+
+        result.emplace_back ( Office (Office::Id(id), Point{x, y}, Offset{offsetx, offsety}) );
+    }
+
+    return result;
+}
+
+
+model::Game LoadGame(const std::filesystem::path& json_path) {
+    // Загрузить содержимое файла json_path, например, в виде строки
+    // Распарсить строку как JSON, используя boost::json::parse
+    // Загрузить модель игры из файла
+    model::Game game;
+    std::string temp{};
+    std::string res{};
+    std::fstream in(json_path);
+    
+    if (in.is_open())
+    {
+        while (std::getline(in, temp))
+        {
+            res += temp;
+        }
+    }
+    in.close(); 
+
+   
+    auto value = json::parse(res);
+    //model::Map(Id id, std::string name)
+    if (!value.is_object()) { throw ("Input Error JSON bad format");}
+    if (value.as_object().find ("maps") == value.as_object().end()) {throw ("Input Error JSON no maps");} 
+    //далее вектор мапов value.as_object().at ("maps")
+    if (!value.as_object().at ("maps").is_array()) {throw ("Input Error JSON - no array after map");}
+    auto& vect_maps = value.as_object().at ("maps").as_array();
+    for (const auto& gamemap : vect_maps) {
+        if (!gamemap.is_object()) {throw ("Input Error JSON");}
+        std::string id{};
+        std::string name{};
+        json::array roads {};
+        json::array buildings {};
+        json::array offices {};
+
+        for (const auto& [key, val] :gamemap.as_object()) {
+            if (key == "id") {id = val.as_string();}
+            else if (key == "name") {name = val.as_string();}
+            else if (key == "roads") {
+                roads = val.as_array();
+                if (roads.size() < 1) {throw ("Input Error JSON - roads empty");}
+            }
+            else if (key == "buildings") { buildings = val.as_array();}
+            else if (key == "offices") {offices = val.as_array();}
+        }
+        Map map (Map::Id{std::move(id)},std::move(name));
+        for (const auto& road:ParseRoads(roads)) {
+            map.AddRoad (road);
+        }
+        for (const auto& buil:ParseBuildings(buildings)) {
+            map.AddBuilding(buil);
+        }
+        for (const auto& off:ParseOffices(offices)) {
+            map.AddOffice(off);
+        }
+
+        game.AddMap(std::move(map));
+    }
+    
+    return game;
+}
+
+
+boost::json::array GetJsonRoads (const model::Map& gamemap) {
+    boost::json::array val;
+    for (const auto& rd : gamemap.GetRoads()) {
+        boost::json::object obj;
+        obj["x0"] = rd.GetStart().x;
+        obj["y0"] = rd.GetStart().y;
+        if (rd.IsVertical()) {
+            obj["y1"] = rd.GetEnd().y;
+        } else {
+            obj["x1"] = rd.GetEnd().x;
+        }
+        val.push_back(std::move(obj));
+
+    }
+    return val;
+}
+
+boost::json::array GetJsonBuildings (const model::Map& gamemap) {
+    boost::json::array val;
+    for (const auto& house : gamemap.GetBuildings()) { 
+        boost::json::object obj;
+        obj["x"] = house.GetBounds().position.x;
+        obj["y"] = house.GetBounds().position.y;
+        obj["w"] = house.GetBounds().size.width;
+        obj["h"] = house.GetBounds().size.height;
+        val.push_back(std::move(obj));
+    }
+    return val;
+}
+
+boost::json::array GetJsonOffices (const model::Map& gamemap) {
+        boost::json::array val;
+    for (const auto& off : gamemap.GetOffices()) { 
+        boost::json::object obj;
+        obj["id"] = *(off.GetId());
+        obj["x"] = off.GetPosition().x;
+        obj["y"] = off.GetPosition().y;
+        obj["offsetX"] = off.GetOffset().dx;
+        obj["offsetY"] = off.GetOffset().dy;
+        val.push_back(std::move(obj));
+    }
+    return val;
+}
+
+
+  // "offices": [
+                    //     { "id": "o0", "x": 40, "y": 30, "offsetX": 5, "offsetY": 0 }
+                    // ]
+
+}  // namespace json_loader
+
---
+++ sprint1/problems/final_task/solution/src/request_handler.h
@@ -0,0 +1,109 @@
+#pragma once
+#include "http_server.h"
+#include "model.h"
+
+namespace http_handler {
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace json = boost::json;
+// Запрос, тело которого представлено в виде строки
+using StringRequest = http::request<http::string_body>;
+// Ответ, тело которого представлено в виде строки
+using StringResponse = http::response<http::string_body>;
+using namespace std::literals;
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;
+    constexpr static std::string_view JSON = "application/json"sv;
+};
+
+class RequestHandler {
+public:
+    explicit RequestHandler(model::Game& game)
+        : game_{game} {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+
+    // Создаёт StringResponse с заданными параметрами
+    StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                    bool keep_alive,
+                                    std::string_view content_type = ContentType::JSON) {
+        StringResponse response(status, http_version);
+        response.set(http::field::content_type, content_type);
+        response.body() = body;
+        response.content_length(body.size());
+        response.keep_alive(keep_alive);
+        return response;
+    }
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        
+        // Обработать запрос request и отправить ответ, используя send
+        if (req.method_string() == "GET") {
+            const auto text_response = [&](http::status status, std::string_view text) {
+                return MakeStringResponse(status, text, req.version(), req.keep_alive());
+            };
+            std::string target = std::string(req.target());
+            
+            if (target == "/api/v1/maps") {
+                json::array arr;
+                for (const auto& gamemap : game_.GetMaps()) {
+                    boost::json::object obj;
+                    
+                    obj[boost::json::string {"id"}] = boost::json::string {*gamemap.GetId()};
+                    obj[boost::json::string {"name"}] = gamemap.GetName();
+                    
+                    //obj[*gamemap.GetId()] = gamemap.GetName();
+                    arr.push_back(obj);
+                }
+                std::string res = json::serialize(arr);
+                //send = text_response(http::status::ok, res );
+                //boost::beast::bind_front_handler(send, text_response(http::status::ok, res));
+                send(text_response(http::status::ok, res));
+
+            }
+            else if (target.starts_with ("/api/v1/maps/") && target.size() > 13) {
+                std::string map_name {target.substr(13)};
+                //auto iter_map = game_.GetMaps().find(map_name);
+                const auto ptr_map  = (game_.FindMap(model::Map::Id{map_name}));
+                if (ptr_map != nullptr) {
+                    boost::json::object val;
+                    val["id"] = *(ptr_map->GetId());
+                    val["name"] = ptr_map->GetName();
+                    val["roads"] = json_loader::GetJsonRoads(*ptr_map);
+                    val["buildings"] = json_loader::GetJsonBuildings(*ptr_map);
+                    val["offices"] = json_loader::GetJsonOffices(*ptr_map);
+                    //boost::beast::bind_front_handler(send, text_response(http::status::ok, json::serialize(val)));
+                    send(text_response(http::status::ok, json::serialize(val)));
+                }
+                else {
+                    boost::json::object val;
+                    val["code"] = "mapNotFound";
+                    val["message"] = "Map not found";
+                    //boost::beast::bind_front_handler(send, text_response(http::status::not_found, json::serialize(val)));
+                    send(text_response(http::status::not_found, json::serialize(val)));
+                }
+
+            }
+            else {
+                boost::json::object val;
+                val["code"] = "badRequest";
+                val["message"] = "Bad request";
+                //boost::beast::bind_front_handler(send, text_response(http::status::bad_request, json::serialize(val)));
+                send(text_response(http::status::bad_request, json::serialize(val)));
+            }
+        }
+
+        
+    }
+
+private:
+    model::Game& game_;
+};
+
+}  // namespace http_handler
