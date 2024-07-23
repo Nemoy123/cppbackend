30fa0a1c8cb0b200c65df7c3894952ae03638144
Nemoy123 <alipkin@yandex.ru>
2024-07-23 11:18:36 +0300

    v4

--- sprint2/problems/server_logging/solution/src/main.cpp
+++ sprint2/problems/server_logging/solution/src/main.cpp
@@ -76,8 +76,8 @@ int main(int argc, const char* argv[]) {
             // send = std::move(send_f);
             
         });
-        
-        LogInfoMessage ("Server has started");
+        std::cout << "Server has started"sv << std::endl;
+        //LogInfoMessage ("Server has started");
         StartServer(port, address.to_string());
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
         //std::cout << "Server has started..."sv << std::endl;
