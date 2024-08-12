4cfeef623dc1b0e4d8b3a2cf0168786eee2c2fa2
Nemoy123 <alipkin@yandex.ru>
2024-08-13 00:23:09 +0300

    s1

---
+++ sprint2/problems/command_line/solution/src/gamesession.cpp
@@ -0,0 +1,26 @@
+#include "gamesession.h"
+
+
+std::string Dog::GetIdString() const {return std::to_string(id_);}
+uint64_t Dog::GetId() const {return id_;}
+const std::string& Dog::GetName() const {return name_;}
+
+uint64_t GameSession::AddDog (const std::string& name) {
+            auto new_dog = std::make_shared <Dog>(Dog(name));
+            auto id = new_dog->GetId();
+            dogs_[id] = std::move (new_dog);
+            return id;
+}
+
+std::shared_ptr <Dog> GameSession::GetDog (uint64_t id_dog) const {
+            return dogs_.contains(id_dog) ? dogs_.at(id_dog) : nullptr;
+}
+
+std::shared_ptr<Player> Players::AddPlayer (uint64_t dog_id, std::string& map_id) {
+    map_players_[std::pair{dog_id, map_id}] = std::make_shared<Player>();
+    return map_players_[std::pair{dog_id, map_id}];
+}
+
+std::shared_ptr<Player> Players::FindByDogIDAndMapID (uint64_t dog_id, Map::Id& map_id) const {
+    return map_players_.at(std::pair{dog_id, *map_id});
+}
---
+++ sprint2/problems/command_line/solution/src/token.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <random>
+#include <sstream>
+#include <ios>
+#include "tagged.h"
+#include <memory>
+
+namespace detail {
+struct Token {
+    Token (const uint64_t& dog, const std::string& token_string)
+        : id_dog_(dog)
+        , token_string_(token_string)
+    {}
+    uint64_t id_dog_{};
+    std::string token_string_{};
+    bool operator== (const Token& b) const  {
+        return id_dog_ == b.id_dog_ && token_string_==b.token_string_;
+    }
+};
+}  // namespace detail
+
+
+
+
+class PlayerTokens {
+    public:
+
+    std::string GenerateToken ();
+    
+private:
+    std::random_device random_device_;
+    std::mt19937_64 generator1_{[this] {
+        std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+        return dist(random_device_);
+    }()};
+    std::mt19937_64 generator2_{[this] {
+        std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+        return dist(random_device_);
+    }()};
+    // Чтобы сгенерировать токен, получите из generator1_ и generator2_
+    // два 64-разрядных числа и, переведя их в hex-строки, склейте в одну.
+    // Вы можете поэкспериментировать с алгоритмом генерирования токенов,
+    // чтобы сделать их подбор ещё более затруднительным
+    uint64_t GenerateFunction ();
+}; 
+
