47b28c21a2cbaf21559247ad28d60ae6a667c3a6
Nemoy123 <alipkin@yandex.ru>
2024-09-03 19:28:47 +0300

    gen2

--- sprint3/problems/gen_objects/solution/src/gamesession.cpp
+++ sprint3/problems/gen_objects/solution/src/gamesession.cpp
@@ -28,19 +28,24 @@ std::shared_ptr<Player> Players::FindByDogIDAndMapID (uint64_t dog_id, Map::Id& 
 void GameSession::AddLoot(int num) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distr(0, gamemap_ptr_->GetLootDescription().size());
-    std::uniform_int_distribution<> road_distr(0, gamemap_ptr_->GetRoads().size());
+    std::uniform_int_distribution<> distr(0, (gamemap_ptr_->GetLootDescription().size())-1);
+    
     for (auto i =0; i < num; ++i) {
+        
         Loot loot;
         loot.type = distr(gen);
+        std::uniform_int_distribution<> road_distr(0, ((gamemap_ptr_->GetRoads().size())-1));
         auto* road_ptr = &(gamemap_ptr_->GetRoads().at(road_distr(gen)));
         
         if (road_ptr->IsHorizontal()) {
-            std::uniform_int_distribution<> road_coor(0, road_ptr->GetEnd().x);
+            std::uniform_int_distribution<> road_coor(std::min(road_ptr->GetStart().x, road_ptr->GetEnd().x), 
+                                                      std::max(road_ptr->GetStart().x, road_ptr->GetEnd().x));
             loot.y = road_ptr->GetStart().y;
             loot.x = road_coor(gen);
         } else {
-            std::uniform_int_distribution<> road_coor(0, road_ptr->GetEnd().y);
+            
+            std::uniform_int_distribution<> road_coor(std::min(road_ptr->GetStart().y, road_ptr->GetEnd().y), 
+                                                      std::max(road_ptr->GetStart().y, road_ptr->GetEnd().y));
             loot.x = road_ptr->GetStart().x;
             loot.y = road_coor(gen);
         }
